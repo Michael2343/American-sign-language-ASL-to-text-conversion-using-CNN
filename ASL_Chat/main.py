327916c4c22f1592c724d5a419ec4cb8a893bd89
Michael2343 <mohayon402@gmail.com>
2024-01-30 18:15:55 +0200

    change

--- ASL_Chat/main.py
+++ ASL_Chat/main.py
@@ -51,5 +51,5 @@ if __name__ == "__main__":
         widgetList.show()
         app.exec_()
     except Exception as e:
-        print("Exception: ",str(e))
+        print("Exception:",str(e))
         
